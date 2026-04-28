a5464c380e48e0885bcdc517519e5cb26d6759bb
mingxuanwu175-afk <mingxuanwu175@gmail.com>
2026-04-29 03:32:13 +0800

    first commit: hello.py running

---
+++ hello.py
@@ -0,0 +1,9 @@
+name = "edubridge"
+version = 1
+print(f"Project: {name}")
+print(f"Version: {version}")
+
+criteria = ["A1", "A2", "B1", "B2"]
+for c in criteria:
+    print(f"Criterion: {c}")
+          
